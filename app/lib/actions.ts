692b46d9123964bc46851653daf3d202f3e5e868
songjiahao <songjiahao@yofish.com>
2024-07-26 11:45:05 +0800

    feat: finish

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,9 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+ 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string({
@@ -38,6 +41,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
             message: "Missing Fields. Failed to Create Invoice.",
         };
     }
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split("T")[0];
     try {
@@ -46,7 +50,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
     } catch (error) {
-        return "Database Error: Failed to Create Invoice.";
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+          };
     }
 
     revalidatePath("/dashboard/invoices");
@@ -85,3 +91,22 @@ export async function deleteInvoice(id: string) {
 
     revalidatePath("/dashboard/invoices");
 }
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
